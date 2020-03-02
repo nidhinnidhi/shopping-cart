fb35d1f9e896bc5fa06b35dfa86c0fe0b67cd355
nidhin <nidhin.nanu@gmail.com>
2020-03-02 15:28:02 +0530

    change in the gitignore

---
+++ build/precache-manifest.94dac84ebf20794f3749771019ec8712.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd966b47d1d8f5f1e28488247e6327c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29347482059e858dd085",
+    "url": "/static/css/main.8c463643.chunk.css"
+  },
+  {
+    "revision": "9ee22a84200ad0f7922b",
+    "url": "/static/js/2.90dfa12a.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.90dfa12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29347482059e858dd085",
+    "url": "/static/js/main.04c7f3d4.chunk.js"
+  },
+  {
+    "revision": "b4b0979611c46f507239",
+    "url": "/static/js/runtime-main.e609aea4.js"
+  },
+  {
+    "revision": "062b83ffa03d019bcbfc92243e20019f",
+    "url": "/static/media/Verdana-Bold.062b83ff.woff2"
+  },
+  {
+    "revision": "35ea96d0cb54665fad2c70141a3956a1",
+    "url": "/static/media/Verdana-Bold.35ea96d0.woff"
+  },
+  {
+    "revision": "97648a3addf3d227d3128c448762cece",
+    "url": "/static/media/Verdana.97648a3a.woff"
+  },
+  {
+    "revision": "ef0ee9db7c0433de26f70d989298d5c5",
+    "url": "/static/media/Verdana.ef0ee9db.woff2"
+  },
+  {
+    "revision": "7e281f6944063656a891db66aeaacc06",
+    "url": "/static/media/close.7e281f69.svg"
+  },
+  {
+    "revision": "40c2064c4c4c3f33a8898ce3db5779a1",
+    "url": "/static/media/loading.40c2064c.svg"
+  },
+  {
+    "revision": "1df0367279b6cb0c85557ab2f18707a2",
+    "url": "/static/media/search.1df03672.svg"
+  },
+  {
+    "revision": "36f48de045cc094ef449a29e65471f93",
+    "url": "/static/media/shopping-cart.36f48de0.svg"
+  },
+  {
+    "revision": "125cb40413432d7cd65caee638c9e731",
+    "url": "/static/media/star.125cb404.svg"
+  }
+]);
